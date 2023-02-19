6efdaf43feb442f68ac478c061a8b701a2223d71
Justin Kopepasah <justin@kopepasah.com>
2023-02-18 23:36:06 -0600

    Hello World.

---
+++ lib/Assets.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Asset management.
+ *
+ * @package StatusBoards
+ *
+ * @author DualFocus <support@dualfocus.dev>
+ */
+
+namespace DualFocus\StatusBoards;
+
+/**
+ * Asset management class.
+ */
+class Assets extends Plugin {
+
+	/**
+	 * Class constructor.
+	 */
+	public function __construct() {}
+}
